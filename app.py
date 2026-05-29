0d77a68cc32f0b6a03a50af7544d14073c91f086
Joginder Kumar Sharma <joginderkumarsharma58@gmail.com>
2026-05-29 15:05:03 +0530

    Updated home page

--- app.py
+++ app.py
@@ -4,11 +4,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! This is my first Cloud Computing App."
+    return """
+    <h1>Welcome to My Cloud App</h1>
+    <p>This app is running on Render Cloud.</p>
+    <a href='/about'>About</a>
+    """
 
 @app.route("/about")
 def about():
-    return "This app can run on cloud server."
+    return """
+    <h2>About Me</h2>
+    <p>My first cloud computing project deployed with Flask and Render.</p>
+    """
 
 if __name__ == "__main__":
     app.run(debug=True)
